bf47fa9399e474f07af0277d685e4ba3478d1e23
Inacio Kunz <inacio.kunz@gmail.com>
2026-01-15 06:01:32 -0300

    personalização

--- src/App.js
+++ src/App.js
@@ -8,37 +8,37 @@ function App() {
 
   const times = [
     {
-      nome: 'Programação',
+      nome: 'Desenvolvedores',
       corPrimaria: '#57C278',
       corSecundaria: '#D9F7E9'
     },
     {
-      nome: 'Front-End',
+      nome: 'Vendas',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF'
     },
     {
-      nome: 'Data Science',
+      nome: 'Marketing',
       corPrimaria: '#A6D157',
       corSecundaria: '#F0F8E2'
     },
     {
-      nome: 'Devops',
+      nome: 'Administração',
       corPrimaria: '#E06B69',
       corSecundaria: '#FDE7E8'
     },
     {
-      nome: 'UX e Design',
+      nome: 'Jurídico',
       corPrimaria: '#DB6EBF',
       corSecundaria: '#FAE9F5'
     },
     {
-      nome: 'Mobile',
+      nome: 'Gestão de Pessoas',
       corPrimaria: '#FFBA05',
       corSecundaria: '#FFF5D9'
     },
     {
-      nome: 'Inovação e Gestão',
+      nome: 'Diretoria',
       corPrimaria: '#FF8A29',
       corSecundaria: '#FFEEDF'
     }
